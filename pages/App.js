d89130f88b323239eacc9435834f82d819c7a958
PaulBlanco29 <joevyblanco29@gmail.com>
2023-10-12 06:44:10 -0400

    firstcommit

---
+++ pages/App.js
@@ -0,0 +1,22 @@
+// Import necessary modules
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} /> {/* This route handles 404 errors */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the React Router Scavenger Hunt!</h1>
+      <p>Start your journey and follow the clues to explore React Router concepts.</p>
+      <a href="/task1">Start the hunt!</a>
+    </div>
+  )
+}
+
+export default Home
